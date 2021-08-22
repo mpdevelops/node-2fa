4c5f8e525dfee96c56db55651b150728baedeba1
Mike Paiva <mikepaiva@Mikes-MacBook-Air.local>
2021-08-22 16:03:17 -0400

    added verify + validate routes, tested to ensure works

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
 
 const app = express();
 
+app.use(express.json());
+
 const db = new JsonDB(new Config('myDB', true, false, '/'));
 
 app.get('/api', (req, res) => res.json({ message: 'Hello World' }));
@@ -18,10 +20,66 @@ app.post('/api/register', (req, res) => {
     const path = `/user/${id}`;
     const temp_secret = speakeasy.generateSecret();
     db.push(path, { id, temp_secret });
-    res.json({ id, secret: temp_secret });
+    res.json({ id, secret: temp_secret.base32 });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: 'Error generating the secret' });
+  }
+});
+
+// verify token and make secret permanent
+app.post('/api/verify', (req, res) => {
+  const { token, userId } = req.body.token;
+
+  try {
+    const path = `/user/${userId}`;
+    const user = db.getData(path);
+
+    const { base32: secret } = user.temp_secret;
+
+    const verified = speakeasy.totp.verify({
+      secret,
+      encoding: 'base32',
+      token,
+    });
+
+    if (verified) {
+      db.push(path, { id: userId, secret: user.temp_secret });
+      res.json({ verified: true });
+    } else {
+      res.json({ verified: false });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error finding user' });
+  }
+});
+
+// validate token
+app.post('/api/validate', (req, res) => {
+  const { token, userId } = req.body.token;
+
+  try {
+    const path = `/user/${userId}`;
+    const user = db.getData(path);
+
+    const { base32: secret } = user.secret;
+
+    const tokenValidates = speakeasy.totp.verify({
+      secret,
+      encoding: 'base32',
+      token,
+      window: 1,
+    });
+
+    if (tokenValidates) {
+      res.json({ validated: true });
+    } else {
+      res.json({ validated: false });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error validating' });
   }
 });
 
