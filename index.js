9582d6bfe247961ef556fe0ff71cc73583a42e53
Mike Paiva <mikepaiva@Mikes-MacBook-Air.local>
2021-08-22 09:53:11 -0400

    user registration route

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ const db = new JsonDB(new Config('myDB', true, false, '/'));
 
 app.get('/api', (req, res) => res.json({ message: 'Hello World' }));
 
+// register user and create temp secret
+app.post('/api/register', (req, res) => {
+  const id = uuid.v4();
+
+  try {
+    const path = `/user/${id}`;
+    const temp_secret = speakeasy.generateSecret();
+    db.push(path, { id, temp_secret });
+    res.json({ id, secret: temp_secret });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error generating the secret' });
+  }
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
