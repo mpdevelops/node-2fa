725d9be1076ec7260d2d1553c22263ad981c4785
Mike Paiva <mikepaiva@Mikes-MacBook-Air.local>
2021-08-21 23:02:15 -0400

    dependency setup

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const speakeasy = require('speakeasy');
+const uuid = require('uuid');
+const { JsonDB } = require('node-json-db');
+const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
 
 const app = express();
+
+const db = new JsonDB(new Config('myDB', true, false, '/'));
 
 app.get('/api', (req, res) => res.json({ message: 'Hello World' }));
 
